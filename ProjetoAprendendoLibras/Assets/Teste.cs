6bbaa2d79574e0f3d7bcb1ccbd06b2603d28252d
Luan Ribeiro da Silva <luan.ribeiro.sv@gmail.com>
2018-02-19 21:34:59 -0300

    Criado as animações do número 0 até 9.

---
+++ ProjetoAprendendoLibras/Assets/Teste.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teste : MonoBehaviour
+{
+
+    private Animator animacao;
+
+    void Start()
+    {
+        animacao = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+			animacao.SetTrigger("Next");
+        }
+    }
+}
