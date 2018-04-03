32f584ae37f38089f143928a11f09d41ebc28047
Luan Ribeiro da Silva <luan.ribeiro.sv@gmail.com>
2018-04-02 23:25:56 -0300

    Implementado alguns testes do jogo de RA.

---
+++ ProjetoAprendendoLibras/Assets/MyProject/Scenes/DetectTrackable.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class DetectTrackable : MonoBehaviour, ITrackableEventHandler
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        GetComponent<TrackableBehaviour>().RegisterTrackableEventHandler(this);
+    }
+
+    public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus,
+                                        TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+			GameObject.Find("Signal"+gameObject.name[gameObject.name.Length-1])
+				.GetComponent<Animator>().SetTrigger("Letter"+gameObject.name[gameObject.name.Length-1]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ ProjetoAprendendoLibras/Assets/MyProject/Scenes/DetectTrigger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectTrigger : MonoBehaviour
+{
+    public GameObject text;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (gameObject.name[gameObject.name.Length-1].Equals(other.gameObject.name[other.gameObject.name.Length-1]))
+            text.GetComponent<TextMesh>().color = Color.blue;
+        else
+            text.GetComponent<TextMesh>().color = Color.red;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        text.GetComponent<TextMesh>().color = new Color32(0, 212, 1, 255);
+    }
+
+}
